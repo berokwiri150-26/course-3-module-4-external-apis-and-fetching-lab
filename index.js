058d107ef6161fdb618d602ff0dc1361b6c21cc3
berokwiri150-26 <berokwiri150@gmail.com>
2026-04-15 23:59:38 +0300

    Redone the whole codebase, for hope of codegrade pass.

--- index.js
+++ index.js
@@ -3,47 +3,33 @@ const weatherApi = "https://api.weather.gov/alerts/active?area=";
 
 // Your code here!
 
-document.addEventListener("DOMContentLoaded", async () => {
 
     // get the elements by IDs 
 
     // create an event listener to trigger the button 
 
-const searchButton = document.getElementById('search-btn');
-const stateInput = document.getElementById('state-input');
-const displayDiv = document.getElementById('alerts-display');
-const errorDiv = document.getElementById('error-message');
 
-searchButton.addEventListener('click', async () => {
-    const state = stateInpute.value.trim();
-    stateInput.value = '';
+function displayerror(message){
+    const displayDiv = document.getElementById("alerts-message");
+    const errorDiv = document.getElementById("error-message");
 
-    if (state) {
-        await fetchWeatherAlerts(state);
-    } else {
-        errorDiv.textcontent = "Enter State Code.";
-        
-    }
-});
+    displayDiv.innerHTML = "";
 
-async function fetchWeatherAlerts(stateAbbr) {
-    const url = fetch(`https://api.weather.gov/alerts/active?area=${STATE_ABBR}()`);
+    errorDiv.textContent = message;
+    errorDiv.classList.remove("hidden");
 }
 
-try{
-    const response = await fetch(url);
-    if (!response.ok) throw new Error (`Error: ${response.error}`);
-    
-    const data = await response.json();
-    console.log(data);
-    displayAlerts(data);
-} catch (error) {
-    console.log("Error", error.message);
-    errorDiv.textContent = error.message;
-    displayDiv.innerHTML = '';
+if(typeof document !== "undefined"){
+    const searchBtn = document.getElementById("search-btn");
+    const stateInput = document.getElementById("state-input");
+
+    searchBtn.addEventListener("click", () => {
+        const stateAbbr = stateInput.value.trim();
+        fetchWeatherAlerts(stateAbbr);
+    });
 }
-});
 
-
-
+if(typeof module !=="undefined"){
+    module.exports = { fetchWeatherAlerts, displayAlerts, displayError};
+}
 
